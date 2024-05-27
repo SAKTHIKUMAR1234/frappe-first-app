dd901efaab88baeef1842ed886bbe74213f6b77e
SAKTHI KUMAR PUGALENTHY <sakthi123msd@gmail.com>
2024-05-27 12:05:45 +0530

    initial commit

---
+++ demo_app/programming/doctype/client_side_scripting/client_side_scripting.js
@@ -0,0 +1,19 @@
+
+frappe.ui.form.on("Client Side Scripting", {
+	// refresh(frm) {
+    //     // frappe.msgprint("Hi The page is refreshed")
+    //     // frappe.throw('This is an error')
+	// },
+    // onload(frm){
+    //     frappe.msgprint("The page is loading")
+    // },
+    // validate(frm){
+    //     frappe.msgprint("Page is saved")
+    // },
+    // family_details_on_form_rendered:function(frm){
+    //     frappe.msgprint('Child table is opened')
+    // },
+    before_submit:function(frm){
+        frappe.thro("Gojsoij")
+    }
+});
